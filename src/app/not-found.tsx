33684abf7cf330bbf34ecbbf1bdbe2e0f8f38425
azizleyla <azizleyla01@gmail.com>
2026-02-26 00:12:18 +0400

    redeploy

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
       <h2 className="text-2xl font-semibold mb-4">Səhifə tapılmadı</h2>
       <p className="mb-6">
-        Üzr istəyirik, axtardığınız səhifə mövcud deyil.
+        Üzr istəyirik, axtardığınız səhifə mövcud deyil..
       </p>
       <Link
         href="/"
